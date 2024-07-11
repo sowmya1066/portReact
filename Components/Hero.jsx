df1db39ff7247681dff2ac9b8bbcaabd6572f553
Sowmya G S <sowmyags1066@gmail.com>
2024-07-11 13:54:46 +0530

    hero section

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -1,12 +1,12 @@
 import "../src/css/hero.css";
-import Sowmya from "../assets/sowmya.png";
+// import Sowmya from "../assets/sowmya.png";
 
 export default function Hero() {
   return (
     <>
       <div className="hero_main">
-        <img src={Sowmya} className="hero--img" />
-        <div className="hero_info">
+        {/* <img src={Sowmya} className="hero--img" /> */}
+        {/* <div className="hero_info">
           <h2>Hi there,</h2>
           <h1>I'm Sowmya</h1>
           <div className="hero_para">
@@ -16,7 +16,7 @@ export default function Hero() {
             <span class="btn-text-ct-one">Connect with me</span>
             <span class="btn-text-ct-two">Hey!</span>
           </button>
-        </div>
+        </div> */}
       </div>
     </>
   );
